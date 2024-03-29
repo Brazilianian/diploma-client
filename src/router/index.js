80adc41984c28fc8723912e0a1c5250a4f485fd8
vdolhushyn <vetajika@gmail.com>
2024-03-29 12:47:29 +0200

    feat: added guards for isAdmin, isAuth

--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,32 @@ import HomeView from '@/views/HomeView.vue'
 import RegistrationView from '@/views/RegistationView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import { auth } from '@/store/auth_store.js'
+
+const MAIN_NAME = "main"
+const LOGIN_NAME = "login"
+const REGISTRATION_NAME = "registration"
+const ADMIN = "ADMIN"
+
+const WHITE_NAME_LIST = [LOGIN_NAME, REGISTRATION_NAME]
+const ADMIN_NAME_LIST = [ADMIN]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: MAIN_NAME,
       component: HomeView
     },
     {
       path: '/login',
-      name: 'login',
+      name: LOGIN_NAME,
       component: LoginView
     },
     {
       path: '/registration',
-      name: 'registration',
+      name: REGISTRATION_NAME,
       component: RegistrationView
     },
     {
@@ -28,6 +37,21 @@ const router = createRouter({
       component: ProfileView
     }
   ]
+})
+
+
+router.beforeEach((to) => {
+  const isAuth = auth.state.isAuth
+  if (!isAuth && ! WHITE_NAME_LIST.includes(to.name)) {
+    return { name: LOGIN_NAME }
+  }
+})
+
+router.beforeEach((to) => {
+  const isAdmin = auth.state.isAdmin
+  if (!isAdmin && ! ADMIN_NAME_LIST.includes(to.name)) {
+    return { name: MAIN_NAME }
+  }
 })
 
 export default router

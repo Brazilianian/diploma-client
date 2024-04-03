a9d4fdf283f6624c557d0ede3b5b37e5733d6bd7
vdolhushyn <vetajika@gmail.com>
2024-04-03 10:19:27 +0300

    fix: router guard loading bug

--- src/router/index.js
+++ src/router/index.js
@@ -3,31 +3,73 @@ import HomeView from '@/views/HomeView.vue'
 import RegistrationView from '@/views/RegistationView.vue'
 import LoginView from '@/views/LoginView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import { auth } from '@/store/auth_store.js'
+import AdminView from '@/views/AdminView.vue'
+
+const MAIN_NAME = 'main'
+const LOGIN_NAME = 'login'
+const REGISTRATION_NAME = 'registration'
+const ADMIN = 'ADMIN'
+
+const WHITE_NAME_LIST = [LOGIN_NAME, REGISTRATION_NAME]
+const ADMIN_NAME_LIST = [ADMIN]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: MAIN_NAME,
       component: HomeView
     },
     {
       path: '/login',
-      name: 'login',
+      name: LOGIN_NAME,
       component: LoginView
     },
     {
       path: '/registration',
-      name: 'registration',
+      name: REGISTRATION_NAME,
       component: RegistrationView
     },
     {
       path: '/profile',
       name: 'profile',
       component: ProfileView
+    },
+    {
+      path: '/admin',
+      name: ADMIN,
+      component: AdminView
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (WHITE_NAME_LIST.includes(to.name)) {
+    return next()
+  }
+
+  const isAuth = auth.state.isAuth
+  const isAdmin = auth.state.isAdmin
+
+  if (ADMIN_NAME_LIST.includes(to.name)) {
+    // Trying to go to the admin urls
+
+    if (isAdmin) {
+      return next()
+    }
+
+    return next({name: MAIN_NAME})
+  }
+
+  if (isAuth) {
+    // Allow common user
+    return next()
+  }
+
+  // Forbid
+  next({ name: LOGIN_NAME })
 })
 
 export default router
---
+++ src/views/AdminView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  ADMIN
+</template>
+
+<style scoped>
+
+</style>
